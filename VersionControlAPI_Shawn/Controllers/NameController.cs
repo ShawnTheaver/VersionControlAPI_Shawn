6dbe4cd8c4fec7a3cf4c309fb2288a319f252d44
ShawnTheaver <theavesn@mail.uc.edu>
2025-07-20 02:52:19 -0400

    Name Controller added

---
+++ VersionControlAPI_Shawn/Controllers/NameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionControlAPI_Shawn.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Sudharshan Theaver" });
+        }
+    }
+}
